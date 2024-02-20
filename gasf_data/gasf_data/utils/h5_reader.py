87418cf097f443af2e4e11ab85182cb355173373
dfredin <dfredin@uw.edu>
2024-02-19 20:47:15 -0800

    Data injection testing
    Adds file structure for loading LIGO strain and BBH waveforms to prepare for injections.

--- gasf_data/gasf_data/utils/h5_reader.py
+++ gasf_data/gasf_data/utils/h5_reader.py
@@ -61,6 +61,6 @@ class h5_thang():
         data_dict = {}
         with h5py.File(self.file , 'r', locking=False) as h1:
             for item in items:
-                data_dict[item] = h1[item][:]
+                data_dict[item] = h1[item][()]
                     
         return data_dict
