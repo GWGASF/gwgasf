db60c32bb2778535b1e05f4331314a28717c8c0f
dfredin <dfredin@uw.edu>
2024-10-09 21:51:13 -0700

    S3 testing
    Testing aws and kubernetes secret

--- src/libs/utils/s3_helper.py
+++ src/libs/utils/s3_helper.py
@@ -2,13 +2,23 @@ import configparser
 import os
 import s3fs
 
+# def load_s3cmd_credentials():
+#     """Load credentials from the .s3cfg file."""
+#     config = configparser.ConfigParser()
+#     config.read(os.path.expanduser('~/.s3cfg'))  # Read from ~/.s3cfg
+#     access_key = config.get('default', 'access_key')
+#     secret_key = config.get('default', 'secret_key')
+#     host_base = config.get('default', 'host_base')
+#     return access_key, secret_key, host_base
+
 def load_s3cmd_credentials():
-    """Load credentials from the .s3cfg file."""
-    config = configparser.ConfigParser()
-    config.read(os.path.expanduser('~/.s3cfg'))  # Read from ~/.s3cfg
-    access_key = config.get('default', 'access_key')
-    secret_key = config.get('default', 'secret_key')
-    host_base = config.get('default', 'host_base')
+    access_key = os.getenv('AWS_ACCESS_KEY_ID')
+    secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    host_base = os.getenv('AWS_S3_ENDPOINT')
+
+    if not access_key or not secret_key or not host_base:
+        raise ValueError("Missing S3 credentials from environment variables")
+
     return access_key, secret_key, host_base
 
 def create_s3_filesystem():
@@ -19,3 +29,9 @@ def create_s3_filesystem():
         secret=secret_key,
         client_kwargs={'endpoint_url': f'{host_base}'}
     )
+
+
+
+
+
+
